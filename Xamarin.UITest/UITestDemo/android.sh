7c9a8eaafec903dd1af1666faa169627767d2aba
Kent Green <Kent.Green@xamarin.com>
2018-02-20 13:22:46 -0800

    set command ready to be pasted in

--- Xamarin.UITest/UITestDemo/android.sh
+++ Xamarin.UITest/UITestDemo/android.sh
@@ -1,7 +1,7 @@
 # Setup & usage
 # Step 0 Build the UITest project & generate an APK using the "Release" configuration
 # Step 1 Generate an AppCenter upload command and paste it to the variable
-AppCenter_Test_Command='appcenter test run uitest --app "kegr/UITestDemo-1" --devices "kegr/top-4" --app-path pathToFile.apk  --test-series "master" --locale "en_US" --build-dir pathToUITestBuildDir'
+AppCenter_Test_Command='paste upload command here'
 
 # Step 2 Provide the (absolute or relative) path to the IPA
 app_path='com.companyname.UITestDemo.apk'
