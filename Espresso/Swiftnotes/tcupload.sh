702ff3ed414b7ab1576dfc21913aeea104ac2b01
Kent Green <Kent.Green@xamarin.com>
2018-02-28 14:41:06 -0800

    finished Espresso upload scripts

--- Espresso/Swiftnotes/tcupload.sh
+++ Espresso/Swiftnotes/tcupload.sh
@@ -1,15 +1,17 @@
-# xtc test yourAppFile.apk 91e67e70dcdbc92ff86d0595b4e2e051 --devices dcbc14e0 --series "master" --user kent.green@xamarin.com --workspace app/build/outputs/apk
-# 'paste command here'
+# Provide Test Cloud upload command
+TestCloud_Command='paste your command here'
+# Delete 'app/build/outputs/apk' because the script is unable to update it when reading the slashes. The "workspace" variable below will add the path back in.
 
-
-# Provide Test Cloud upload command, path to APK & path to xtc file for the OS.
-TestCloud_Command='xtc test yourAppFile.apk 91e67e70dcdbc92ff86d0595b4e2e051 --devices dcbc14e0 --series "master" --user kent.green@xamarin.com --workspace app/build/outputs/apk'
-app_path='app/build/outputs/apk/debug/app-debug.apk'
-workspace='app/build/outputs/apk/androidTest'
+# provide a path to the APK, the workspace, and the unzipped xtcMac or xtcWin folder.
+apk_path='app/build/outputs/apk/debug/app-debug.apk'
+workspace='--workspace app/build/outputs/apk/androidTest/debug'
+platform='xtcMac/xtc'
 
 # Run using the command "sh tcupload.sh"
 
 # Script injects app_path & platform and executes resulting command
-TestCloud_Command=${TestCloud_Command/'yourAppFile.apk'/$app_path}
+TestCloud_Command=${TestCloud_Command/'--workspace'/$workspace}
+TestCloud_Command=${TestCloud_Command/'yourAppFile.apk'/$apk_path}
 TestCloud_Command=${TestCloud_Command/'xtc'/$platform}
+echo $TestCloud_Command # For debugging only, prints the final command that will be submitted by eval
 eval $TestCloud_Command
--- Espresso/Swiftnotes/upload.sh
+++ Espresso/Swiftnotes/upload.sh
@@ -1,15 +1,14 @@
+# Provide AppCenter/Test upload command
+AppCenter_Test_Command='paste your command here'
 
+# Point to APK & test build-dir 
+apk_path='app/build/outputs/apk/debug/app-debug.apk'
+build_dir='app/build/outputs/apk/androidTest/debug'
 
+# Run using the command "sh tcupload.sh"
 
-
-
-# Provide AppCenter/Test upload command & path to APK.
-AppCenter_Test_Command='paste command here'
-app_path='../swiftnote.apk'
-
-# Run using the command "sh upload.sh"
-Build_TestUpload_Command='mvn -DskipTests -P prepare-for-upload package'
-eval $Build_TestUpload_Command
-
-AppCenter_Test_Command=${AppCenter_Test_Command/'pathToFile.apk'/$app_path}
+# Script injects set values
+AppCenter_Test_Command=${AppCenter_Test_Command/'pathToFile.apk'/$apk_path}
+AppCenter_Test_Command=${AppCenter_Test_Command/'pathToEspressoBuildFolder'/$build_dir}
+echo $AppCenter_Test_Command # For debugging only, prints the final command that will be submitted by eval
 eval $AppCenter_Test_Command
