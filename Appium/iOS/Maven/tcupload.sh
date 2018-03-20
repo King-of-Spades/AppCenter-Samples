eb2d0a82a78399c1ee0de770eac34c4a70966d69
Kent Green <Kent.Green@xamarin.com>
2018-03-20 15:38:02 -0700

    update upload scripts for iOS

--- Appium/iOS/Maven/tcupload.sh
+++ Appium/iOS/Maven/tcupload.sh
@@ -1,6 +1,6 @@
 # Provide Test Cloud upload command, path to APK & path to xtc file for the OS.
 TestCloud_Command='paste command here'
-app_path='../swiftnote.apk'
+app_path='../UITestDemo.ipa'
 platform='xtcMac/xtc'
 
 # Run using the command "sh tcupload.sh"
@@ -8,6 +8,6 @@ Build_Upload_Command='mvn -DskipTests -P prepare-for-upload package'
 eval $Build_Upload_Command
 
 # Script injects app_path & platform and executes resulting command
-TestCloud_Command=${TestCloud_Command/'yourAppFile.apk'/$app_path}
+TestCloud_Command=${TestCloud_Command/'yourAppFile.ipa'/$app_path}
 TestCloud_Command=${TestCloud_Command/'xtc'/$platform}
 eval $TestCloud_Command
--- Appium/iOS/Maven/upload.sh
+++ Appium/iOS/Maven/upload.sh
@@ -1,12 +1,12 @@
-# Provide AppCenter/Test upload command & path to APK.
+# Provide AppCenter/Test upload command & path to IPA.
 AppCenter_Test_Command='paste command here'
-app_path='../swiftnote.apk'
+app_path='../UITestDemo.ipa'
 
 # Run using the command "sh upload.sh"
 Build_TestUpload_Command='mvn -DskipTests -P prepare-for-upload package'
 echo $Build_TestUpload_Command
 eval $Build_TestUpload_Command
 
-AppCenter_Test_Command=${AppCenter_Test_Command/'pathToFile.apk'/$app_path}
+AppCenter_Test_Command=${AppCenter_Test_Command/'pathToFile.ipa'/$app_path}
 echo $AppCenter_Test_Command
 eval $AppCenter_Test_Command
