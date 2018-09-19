fdaec72665e768b8e8ed749947e3676cece793ff
king-of-spades <kegr@microsoft.com>
2018-09-19 10:00:11 -0700

    correct comment typo

--- Xamarin.UITest/UITestDemo/android.sh
+++ Xamarin.UITest/UITestDemo/android.sh
@@ -9,7 +9,7 @@ app_path='precompiledApps/com.companyname.UITestDemo.apk'
 # Step 3 Provide the (absolute or relative) path to the UITest project bin folder
 build_dir='UITestDemo.UITest/bin/Debug'
 
-# Step 4 run using the command "sh ios.sh"
+# Step 4 run using the command "sh android.sh"
 
 # Script injects app_path & build_dir and executes resulting command
 AppCenter_Test_Command=${AppCenter_Test_Command/'pathToFile.apk'/$app_path}
