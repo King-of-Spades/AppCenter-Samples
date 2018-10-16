fafc5a39f071484ffbda48855eac98a2705f4a67
king-of-spades <kegr@microsoft.com>
2018-10-16 15:45:38 -0700

    modified about page

--- Xamarin.UITest/UITestDemo/UITestDemo/Views/AboutPage.xaml.cs
+++ Xamarin.UITest/UITestDemo/UITestDemo/Views/AboutPage.xaml.cs
@@ -6,9 +6,24 @@ namespace UITestDemo
 {
     public partial class AboutPage : ContentPage
     {
+        ItemsViewModel viewModel;
         public AboutPage()
         {
             InitializeComponent();
+            BindingContext = viewModel = new ItemsViewModel();
+            logo1.Rotation = 90;
+        }
+
+        async void logo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewItemPage());
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
         }
     }
 }
