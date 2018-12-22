6d22f5fd7cd41fea07a5b41fc5d09c8a44713154
king-of-spades <kegr@microsoft.com>
2018-12-21 17:03:56 -0800

    Temporarily igonring Android to make it easier to develop iOS tests

--- Xamarin.UITest/FormsGallery/FormsGallery.UITest/CSTests.cs
+++ Xamarin.UITest/FormsGallery/FormsGallery.UITest/CSTests.cs
@@ -7,7 +7,7 @@ using Xamarin.UITest.Queries;
 
 namespace FormsGallery.UITest
 {
-    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.Android)]
     [TestFixture(Platform.iOS)]
     public class CSTests
     {
