3c7e86d92e47053663280d64e8e9d2893149ceea
king-of-spades <kegr@microsoft.com>
2019-02-01 17:50:29 -0800

    Extract out cells, pages & layouts with single content

---
+++ Xamarin.UITest/FormsGallery/FormsGallery.UITest/LayoutsWithSingleContent.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace FormsGallery.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class LayoutsWithSingleContent : HelperMethods
+    {
+        public LayoutsWithSingleContent(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [Test]
+        public void ContentView()
+        {
+            for (int i = 0; i >= 1; i++)
+            {
+                OpenPage("ContentView", i);
+                app.Back();
+            }
+        }
+
+        [Test]
+        public void Frame()
+        {
+            for (int i = 0; i >= 1; i++)
+            {
+                OpenPage("Frame", i);
+                app.Back();
+            }
+        }
+
+        [Test]
+        public void ScrollView()
+        {
+            for (int i = 0; i >= 1; i++)
+            {
+                OpenPage("ScrollView", i);
+                app.ScrollDown();
+                app.Screenshot("Scrolled down");
+                app.ScrollUp();
+                app.Screenshot("Scrolled back up");
+                app.Back();
+            }
+        }
+    }
+}
