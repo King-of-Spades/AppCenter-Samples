bc575c661a9d4980337f265f00f1ad88851eab18
king-of-spades <kegr@microsoft.com>
2019-06-13 15:53:20 -0700

    add demo build scripts to UITestDemo

---
+++ Xamarin.UITest/UITestDemo/Droid/appcenter-post-build.sh
@@ -0,0 +1,9 @@
+# Comments
+
+echo "Hello World! I'm a post-build script!"
+
+echo "I run at the end of your build." 
+
+echo "Documentation: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts/#post-clone"
+
+echo "end post-build script"
