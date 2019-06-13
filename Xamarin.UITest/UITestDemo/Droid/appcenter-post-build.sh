0430a87dce1d08e27468cae15448e84db88656a6
king-of-spades <kegr@microsoft.com>
2019-06-13 16:58:15 -0700

    Update appcenter-post-build.sh

--- Xamarin.UITest/UITestDemo/Droid/appcenter-post-build.sh
+++ Xamarin.UITest/UITestDemo/Droid/appcenter-post-build.sh
@@ -1,9 +1,14 @@
 # Comments
+App_Center_Test_Command='appcenter test run uitest --app "XTCTeam/UITestDemo-1" --devices "XTCTeam/galaxy-s9-and-google-pixels-android-7-dot-1-2-9-dot-0" --app-path "APPCENTER_OUTPUT_DIRECTORY/com.companyname.UITestDemo.apk"  --test-series "master" --locale "en_US" --build-dir "APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/UITestDemo/UITestDemo.UITest/bin/Debug" --async'
 
 echo "Hello World! I'm a post-build script!"
-
 echo "I run at the end of your build." 
-
 echo "Documentation: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts/#post-clone"
+
+echo "Attempting to upload & run test suite using source directory: " $APPCENTER_SOURCE_DIRECTORY
+echo "& output directory: " $APPCENTER_OUTPUT_DIRECTORY
+
+echo $App_Center_Test_Command
+eval $App_Center_Test_Command
 
 echo "end post-build script"
