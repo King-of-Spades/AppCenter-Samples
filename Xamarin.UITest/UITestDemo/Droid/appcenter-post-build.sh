e4419417f70b3a5a75f9317b1aca50acefb904a5
king-of-spades <kegr@microsoft.com>
2019-06-14 10:40:15 -0700

    tweak to try and get variables working

--- Xamarin.UITest/UITestDemo/Droid/appcenter-post-build.sh
+++ Xamarin.UITest/UITestDemo/Droid/appcenter-post-build.sh
@@ -1,5 +1,5 @@
 # Comments
-App_Center_Test_Command='appcenter test run uitest --app "XTCTeam/UITestDemo-1" --devices "XTCTeam/galaxy-s9-and-google-pixels-android-7-dot-1-2-9-dot-0" --app-path "APPCENTER_OUTPUT_DIRECTORY/com.companyname.UITestDemo.apk"  --test-series "master" --locale "en_US" --build-dir "APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/UITestDemo/UITestDemo.UITest/bin/Debug" --async --token API_KEY'
+App_Center_Test_Command='appcenter test run uitest --app "XTCTeam/UITestDemo-1" --devices "XTCTeam/galaxy-s9-and-google-pixels-android-7-dot-1-2-9-dot-0" --app-path $APPCENTER_OUTPUT_DIRECTORY/com.companyname.UITestDemo.apk  --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/UITestDemo/UITestDemo.UITest/bin/Debug --async --token API_KEY'
 
 echo "Hello World! I'm a post-build script!"
 echo "I run at the end of your build." 
