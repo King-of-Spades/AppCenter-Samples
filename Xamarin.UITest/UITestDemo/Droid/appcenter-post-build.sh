750a360bbd84ec3096497181ceed1215286222f4
king-of-spades <kegr@microsoft.com>
2019-06-14 14:54:03 -0700

    Make operational & cleanup

--- Xamarin.UITest/UITestDemo/Droid/appcenter-post-build.sh
+++ Xamarin.UITest/UITestDemo/Droid/appcenter-post-build.sh
@@ -1,25 +1,34 @@
-# Comments
-App_Center_Test_Command='appcenter test run uitest --app "XTCTeam/UITestDemo-1" --devices "XTCTeam/galaxy-s9-and-google-pixels-android-7-dot-1-2-9-dot-0" --app-path $APPCENTER_OUTPUT_DIRECTORY/com.companyname.UITestDemo.apk  --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/UITestDemo/UITestDemo.UITest/bin/Debug --async --token API_KEY --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/UITestDemo/packages/Xamarin.UITest.*/tools'
+# Environment Variables created by App Center
+# $APPCENTER_SOURCE_DIRECTORY
+# $APPCENTER_OUTPUT_DIRECTORY
 
-echo "Hello World! I'm a post-build script!"
-echo "I run at the end of your build." 
-echo "Documentation: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts/#post-clone"
+# Custom Environment Variables
+# $API_KEY
+UITEST_PATH='Xamarin.UITest/UITestDemo/UITestDemo.UITest'
 
-echo "Attempting to upload & run test suite using source directory: " $APPCENTER_SOURCE_DIRECTORY
-echo "Contents: "
-ls $APPCENTER_SOURCE_DIRECTORY
+# DEBUGGING
+# echo "Hello World! I'm a post-build script!"
+# echo "I run at the end of your build." 
+# echo "Documentation: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts/#post-build"
+# echo "Referencing source directory: " $APPCENTER_SOURCE_DIRECTORY
+# echo "Contents: "
+# ls $APPCENTER_SOURCE_DIRECTORY
+# echo "& output directory: " $APPCENTER_OUTPUT_DIRECTORY
+# echo "Contents: "
+# ls $APPCENTER_OUTPUT_DIRECTORY
 
-echo "& output directory: " $APPCENTER_OUTPUT_DIRECTORY
-echo "Contents: "
-ls $APPCENTER_OUTPUT_DIRECTORY
+# Build UITest project
+eval MSBuild $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH -v:q 
 
-echo "contents of UITest directory:" 
-ls $APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/UITestDemo/UITestDemo.UITest
+# DEBUGGING
+# echo "contents of UITest directory:" 
+# ls $APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/UITestDemo/UITestDemo.UITest
+
+# Upload tests
+App_Center_Test_Command='appcenter test run uitest --app "XTCTeam/UITestDemo-1" --devices "XTCTeam/galaxy-s9-and-google-pixels-android-7-dot-1-2-9-dot-0" --app-path $APPCENTER_OUTPUT_DIRECTORY/com.companyname.UITestDemo.apk  --test-series "master" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH/bin/Debug --async --token $API_KEY --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/UITestDemo/packages/Xamarin.UITest.*/tools'
 
 echo $App_Center_Test_Command
-App_Center_Test_Command=${App_Center_Test_Command/'API_KEY'/$API_KEY}
-# eval $App_Center_Test_Command
+eval $App_Center_Test_Command
 
-
-
+# End
 echo "end post-build script"
