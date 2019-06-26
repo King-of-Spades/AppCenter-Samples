4d46d2bfd06e55ddafab82a8846b54c07969d448
Kent Green <King-of-Spades@users.noreply.github.com>
2019-06-26 15:57:32 -0700

    check mono version

--- Xamarin.UITest/UITestDemo/Droid/appcenter-post-clone.sh
+++ Xamarin.UITest/UITestDemo/Droid/appcenter-post-clone.sh
@@ -1,9 +1,10 @@
 # Comments
 
 echo "Hello World! I'm a post-clone script!"
-
 echo "I run immediately after your project has finished cloning."
-
 echo "Documentation: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts/#post-clone"
+
+echo "checking mono --version"
+mono --version
 
 echo "end post-clone script"
