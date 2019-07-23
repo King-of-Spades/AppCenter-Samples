4152b9ff8486d61d6523e42e77418dcb4bbee1e0
Kent Green <kegr@microsoft.com>
2019-07-23 15:49:54 -0700

    initial copy-paste of scripts

---
+++ Xamarin.UITest/FormsGallery/FormsGallery/FormsGallery.iOS/appcenter-post-build.sh
@@ -0,0 +1,37 @@
+# Environment Variables created by App Center
+# $APPCENTER_SOURCE_DIRECTORY
+# $APPCENTER_OUTPUT_DIRECTORY
+# $APPCENTER_BRANCH
+
+# Custom Environment Variables
+# $API_KEY
+# $TEAM_APP
+# $DEVICE_SET
+UITEST_PATH='Xamarin.UITest/FormsGallery/FormsGallery.UITest'
+
+# DEBUGGING
+# echo "Hello World! I'm a post-build script!"
+# echo "I run at the end of your build." 
+# echo "Documentation: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts/#post-build"
+# echo "Referencing source directory: " $APPCENTER_SOURCE_DIRECTORY
+# echo "Contents: "
+# ls $APPCENTER_SOURCE_DIRECTORY
+# echo "& output directory: " $APPCENTER_OUTPUT_DIRECTORY
+# echo "Contents: "
+# ls $APPCENTER_OUTPUT_DIRECTORY
+
+# Build UITest project
+eval MSBuild $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH -v:q 
+
+# DEBUGGING
+# echo "contents of UITest directory:" 
+# ls $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH
+
+# Upload tests
+App_Center_Test_Command='appcenter test run uitest --app $TEAM_APP --devices $DEVICE_SET --app-path $APPCENTER_OUTPUT_DIRECTORY/com.appcenter.FormsGallery.apk  --test-series "gh-$APPCENTER_BRANCH" --locale "en_US" --build-dir $APPCENTER_SOURCE_DIRECTORY/$UITEST_PATH/bin/Debug --async --token $API_KEY --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/Xamarin.UITest/FormsGallery/packages/Xamarin.UITest.*/tools'
+
+echo $App_Center_Test_Command
+eval $App_Center_Test_Command
+
+# End
+echo "end post-build script"
