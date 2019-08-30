80d2388c323d0911bea74179b53d639e6689dc05
Kent Green <kegr@microsoft.com>
2019-08-29 23:19:44 -0700

    precompiled apps

--- Xamarin.UITest/FormsGallery/android.sh
+++ Xamarin.UITest/FormsGallery/android.sh
@@ -1,18 +1,22 @@
 # Setup & usage
 # Step 0 Build the UITest project & generate an APK using the "Release" configuration (or use the provided APK)
 # Step 1 Generate an AppCenter upload command and paste it to the variable
-AppCenter_Test_Command='paste upload command here'
+AppCenter_Test_Command='appcenter test run uitest --app "XTCTeam/Forms-Gallery-Android" --devices "XTCTeam/8-dot-1-9-dot-0-galaxy-note-9-pixel" --app-path pathToFile.apk  --test-series "master" --locale "en_US" --build-dir pathToUITestBuildDir'
 
 # Step 2 Provide the (absolute or relative) path to the apk
-app_path='precompiledApps/com.companyname.UITestDemo.apk'
+app_path='precompiledApps/com.appcenter.FormsGallery.apk'
 
 # Step 3 Provide the (absolute or relative) path to the UITest project bin folder
-build_dir='UITestDemo.UITest/bin/Debug'
+build_dir='FormsGallery.UITest/bin/Debug'
 
-# Step 4 run using the command "sh android.sh"
+# Step 4 add --include argument
+include=' --include "includedDataFile.txt"'
+
+# Step 5 run using the command "sh android.sh"
 
 # Script injects app_path & build_dir and executes resulting command
 AppCenter_Test_Command=${AppCenter_Test_Command/'pathToFile.apk'/$app_path}
 AppCenter_Test_Command=${AppCenter_Test_Command/'pathToUITestBuildDir'/$build_dir}
+AppCenter_Test_Command+=$include
 echo $AppCenter_Test_Command
 eval $AppCenter_Test_Command
