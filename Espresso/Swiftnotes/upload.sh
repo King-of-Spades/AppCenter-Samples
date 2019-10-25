67151250dcdc1cfc65c72513c24b7afd03913839
Kent Green <kegr@microsoft.com>
2019-10-25 16:10:52 -0700

    add gradle wrapper step to upload script

--- Espresso/Swiftnotes/upload.sh
+++ Espresso/Swiftnotes/upload.sh
@@ -5,6 +5,10 @@ AppCenter_Test_Command='paste upload command here'
 apk_path='app/build/outputs/apk/debug/app-debug.apk'
 build_dir='app/build/outputs/apk/androidTest/debug'
 
+# Build the project & Test APK files using gradle wrapper file
+./gradlew assembleDebug
+./gradlew assembleDebugAndroidTest
+
 # Run using the command "sh upload.sh"
 
 # Script injects set values
@@ -12,3 +16,6 @@ AppCenter_Test_Command=${AppCenter_Test_Command/'pathToFile.apk'/$apk_path}
 AppCenter_Test_Command=${AppCenter_Test_Command/'pathToEspressoBuildFolder'/$build_dir}
 echo $AppCenter_Test_Command 
 eval $AppCenter_Test_Command
+
+
+
